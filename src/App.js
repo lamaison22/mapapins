62c17775a529501f6f6040d5b5c4265a900b8ddc
João Lamaison <90867981+lamaison22@users.noreply.github.com>
2024-03-05 10:19:05 -0300

    testando leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Icon, divIcon, point } from "leaflet";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 function App() {
+  const markers = [
+    {
+      geocode: [48.86, 2.3522],
+      popUp: "Harro je susi le oasdo",
+    },
+    {
+      geocode: [48.85, 2.3522],
+      popUp: "Harro je susi le oasd 2 ",
+    },
+    {
+      geocode: [48.855, 2.34],
+      popUp: "Harro je susi le oasdo 3",
+    },
+  ];
+  const customIcon = new Icon({
+    iconUrl: "pin.png",
+    iconSize: [38, 38],
+  });
+
+  const createCustomClusterIcon = (cluster)=>{
+    return new divIcon({
+      html:`<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className:"custom-marker-cluster",
+      iconSize:point(33,33,true)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <MarkerClusterGroup chunkedLoading iconCreateFunction={createCustomClusterIcon}>
+          {markers.map((marker) => (
+            <Marker position={marker.geocode} icon={customIcon}>
+              <Popup>{marker.popUp}</Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </MapContainer>
     </div>
   );
 }
